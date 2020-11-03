6a2489be0273897ede9dc14c22c81a8772f5df6f
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-04 03:23:54 +0500

    Adding files

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -7,9 +7,7 @@ def notifyBuild(String buildStatus = 'STARTED') {
   def colorCode = '#FF0000'
   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
   def summary = "${subject} (${env.BUILD_URL})"
-  def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-
+  
   // Override default values based on build status
   if (buildStatus == 'STARTED') {
     color = 'YELLOW'
