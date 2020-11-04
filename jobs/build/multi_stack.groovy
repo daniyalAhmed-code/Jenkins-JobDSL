d5807d382e8e6f26c9072589ddee169d5c4795a3
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-05 00:04:34 +0500

    Adding task1.groovy

---
+++ jobs/build/multi_stack.groovy
@@ -0,0 +1,21 @@
+multibranchPipelineJob('multi-test') {
+    branchSources {
+        git {
+            id('123456789') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/daniyalAhmed-code/Jenkins-JobDsl.git')
+            includes('devTest')
+            excludes('main')
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+               scriptPath("pipeline/PrintText2.groovy")
+    }
+    }
+    
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(2)
+        }
+    }
+}
