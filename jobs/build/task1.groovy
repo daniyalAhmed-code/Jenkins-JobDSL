52b27ae2494757adad4b22a43baa59e5eb1565a7
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-05 00:22:05 +0500

    Adding files

--- jobs/build/task1.groovy
+++ jobs/build/task1.groovy
@@ -1,16 +1,22 @@
-multibranchPipelineJob('multi-test') {
+multibranchPipelineJob('multi_test') {
     branchSources {
         git {
             id('123456789') // IMPORTANT: use a constant and unique identifier
-            remote('https://github.com/daniyalAhmed-code/jenkins-Jcasc.git')
+            remote('https://github.com/daniyalAhmed-code/Jenkins-JobDsl.git')
             includes('*')
-            excludes('main')
+            excludes('devTest main')
         }
     }
+    factory {
+        workflowBranchProjectFactory {
+               scriptPath("pipeline/PrintText2.groovy")
+    }
+    }
+    
+    
     orphanedItemStrategy {
         discardOldItems {
             numToKeep(2)
         }
     }
 }
-
