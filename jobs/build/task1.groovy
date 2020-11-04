2b8e2e798d0ec07e774d064d0ce2633674cb42fc
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-04 23:42:08 +0500

    Adding task1

--- jobs/build/task1.groovy
+++ jobs/build/task1.groovy
@@ -1,1 +1,19 @@
-println("Hello world")
+multibranchPipelineJob('multi-test') {
+    branchSources {
+        git {
+            id('123456789') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/daniyalAhmed-code/jenkins-Jcasc.git')
+            includes('*')
+            excludes('main')
+        }
+    }
+    configure {
+            scriptPath("pipelines/PrintText.groovy")
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(2)
+        }
+    }
+}
+
