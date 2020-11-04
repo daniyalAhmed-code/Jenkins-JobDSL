a8d4b5c07959ea7e79efa6e31a1ba06ac64e02e7
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-04 22:31:00 +0500

    Adding files

--- jobs/build/task1.groovy
+++ jobs/build/task1.groovy
@@ -1,1 +1,17 @@
-println("Hello world")
+multibranchPipelineJob('multi-test') {
+    branchSources {
+        git {
+            id('123456789') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/jenkinsci/job-dsl-plugin.git')
+            credentialsId('github-ci')
+            includes('*')
+            excludes('main')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(2)
+        }
+    }
+}
+
