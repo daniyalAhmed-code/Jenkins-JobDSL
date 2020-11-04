65a158d5ed93971f7c0b28e33a3ee1fa5e69c0da
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-04 23:14:10 +0500

    Adding files

---
+++ jobs/build/dev_task2.groovy
@@ -0,0 +1,16 @@
+multibranchPipelineJob('Dev_task2') {
+    branchSources {
+        git {
+            id('12df-34e-56a-7b89') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/daniyalAhmed-code/jenkins-JobDsl.git')
+            includes('*')
+            excludes('main')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(2)
+        }
+    }
+}
+
