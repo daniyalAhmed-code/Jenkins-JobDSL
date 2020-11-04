cee7424cb09ba9d3dd95204f840c6192844238cc
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-05 00:29:48 +0500

    Adding files

---
+++ jobs/build/mutil_task.groovy
@@ -0,0 +1,22 @@
+multibranchPipelineJob('multi_test') {
+    branchSources {
+        git {
+            id('12-34ab-56fe-789') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/daniyalAhmed-code/Jenkins-JobDsl.git')
+            includes('*')
+            excludes('devTest main')
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+               scriptPath("pipeline/PrintText.groovy")
+    }
+    }
+    
+    
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(2)
+        }
+    }
+}
