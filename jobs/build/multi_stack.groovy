299d8fd086227df18ad053e59d3c84f010293af8
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-05 22:21:23 +0500

    5th Nov

--- jobs/build/multi_stack.groovy
+++ jobs/build/multi_stack.groovy
@@ -1,53 +1,53 @@
-multibranchPipelineJob('multi_test1') {
-    branchSources {
-        git {
-            id('123456789') // IMPORTANT: use a constant and unique identifier
-            remote {
-                github('https://github.com/daniyalAhmed-code/Jenkins-JobDsl.git')
-                refspec('+refs/pull/*:refs/remotes/origin/pr/*')
-            }
-          branch('${sha1}')
-        }
-    }
-     triggers {
-        pullRequest {
-            admin('user_1')
-            triggerPhrase('OK to test')
-            onlyTriggerPhrase()
-            useGitHubHooks()
-            permitAll()
-            autoCloseFailedPullRequests()
-            displayBuildErrorsOnDownstreamBuilds()
-            whiteListTargetBranches(['develop','devTest'])
-            allowMembersOfWhitelistedOrgsAsAdmin()
-            extensions {
-                commitStatus {
-                    context('deploy to staging site')
-                    triggeredStatus('starting deployment to staging site...')
-                    startedStatus('deploying to staging site...')
-                    addTestResults(true)
-                    completedStatus('SUCCESS', 'All is well')
-                }
-            }
-        }
-    }
-    publishers {
-        mergeGithubPullRequest {
-            mergeComment('merged by Jenkins')
-            onlyAdminsMerge()
-        }
-    }
+// multibranchPipelineJob('multi_test1') {
+//     branchSources {
+//         git {
+//             id('123456789') // IMPORTANT: use a constant and unique identifier
+//             remote {
+//                 github('https://github.com/daniyalAhmed-code/Jenkins-JobDsl.git')
+//                 refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+//             }
+//           branch('${sha1}')
+//         }
+//     }
+//      triggers {
+//         pullRequest {
+//             admin('user_1')
+//             triggerPhrase('OK to test')
+//             onlyTriggerPhrase()
+//             useGitHubHooks()
+//             permitAll()
+//             autoCloseFailedPullRequests()
+//             displayBuildErrorsOnDownstreamBuilds()
+//             whiteListTargetBranches(['develop','devTest'])
+//             allowMembersOfWhitelistedOrgsAsAdmin()
+//             extensions {
+//                 commitStatus {
+//                     context('deploy to staging site')
+//                     triggeredStatus('starting deployment to staging site...')
+//                     startedStatus('deploying to staging site...')
+//                     addTestResults(true)
+//                     completedStatus('SUCCESS', 'All is well')
+//                 }
+//             }
+//         }
+//     }
+//     publishers {
+//         mergeGithubPullRequest {
+//             mergeComment('merged by Jenkins')
+//             onlyAdminsMerge()
+//         }
+//     }
 
-    factory {
-        workflowBranchProjectFactory {
-               scriptPath("pipeline/PrintText2.groovy")
-    }
-    }
+//     factory {
+//         workflowBranchProjectFactory {
+//                scriptPath("pipeline/PrintText2.groovy")
+//     }
+//     }
     
     
-    orphanedItemStrategy {
-        discardOldItems {
-            numToKeep(2)
-        }
-    }
-}
+//     orphanedItemStrategy {
+//         discardOldItems {
+//             numToKeep(2)
+//         }
+//     }
+// }
