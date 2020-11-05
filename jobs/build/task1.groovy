b27a034cde03738724bce90cbd78c2a871beaee7
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-05 22:19:35 +0500

    5th Nov

--- jobs/build/task1.groovy
+++ jobs/build/task1.groovy
@@ -1,46 +1,36 @@
-<<<<<<< HEAD
-// multibranchPipelineJob('multi_test') {
-//     branchSources {
-//         git {
-//             id('123456789') // IMPORTANT: use a constant and unique identifier
-//             remote('https://github.com/daniyalAhmed-code/Jenkins-JobDsl.git')
-//             includes('develop')
-//             excludes('main')
-=======
-
-
-multibranchPipelineJob('multi_test') {
-    branchSources {
+job('example') {
+    scm {
         git {
-            id('123456789') // IMPORTANT: use a constant and unique identifier
-            remote('https://github.com/daniyalAhmed-code/Jenkins-JobDsl.git')
-            includes('develop')
-            excludes('main')
->>>>>>> c69197d73d859281f471c795d060bb55d46f1636
-
-//         }
-//     }
-//     factory {
-//         workflowBranchProjectFactory {
-//                scriptPath("pipeline/PrintText.groovy")
-//     }
-//     }
-    
-<<<<<<< HEAD
-//     orphanedItemStrategy {
-//         discardOldItems {
-//             numToKeep(2)
-//         }
-//     }
-// }
-=======
-
-  
-    orphanedItemStrategy {
-        discardOldItems {
-            numToKeep(2)
+            remote {
+                github('daniyalAhmed-code/Jenkins-JobDsl')
+                refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+            } 
+            branch('${sha1}')
+        }
+    }
+    triggers {
+        githubPullRequest {
+            admin('daniyalAhmed-code')
+            orgWhitelist('my_github_org')
+            cron('H/5 * * * *')
+            triggerPhrase('OK to test')
+            onlyTriggerPhrase()
+            useGitHubHooks()
+            permitAll()
+            autoCloseFailedPullRequests()
+            allowMembersOfWhitelistedOrgsAsAdmin()
+            extensions {
+                commitStatus {
+                    context('deploy to staging site')
+                    triggeredStatus('starting deployment to staging site...')
+                    startedStatus('deploying to staging site...')
+                    statusUrl('http://mystatussite.com/prs')
+                    completedStatus('SUCCESS', 'All is well')
+                    completedStatus('FAILURE', 'Something went wrong. Investigate!')
+                    completedStatus('PENDING', 'still in progress...')
+                    completedStatus('ERROR', 'Something went really wrong. Investigate!')
+                }
+            }
         }
     }
 }
->>>>>>> c69197d73d859281f471c795d060bb55d46f1636
-
