3364ffc0c34b4b5296123b8d93813e11c4e13dd8
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-06 22:37:15 +0500

    Adding files

--- jobs/build/multi_stack.groovy
+++ jobs/build/multi_stack.groovy
@@ -3,6 +3,7 @@ multibranchPipelineJob('multi_test1') {
         git {
             id('123456789') // IMPORTANT: use a constant and unique identifier
             remote ('https://github.com/daniyalAhmed-code/Jenkins-JobDsl.git')
+             includes('develop')
         }
     }
     
