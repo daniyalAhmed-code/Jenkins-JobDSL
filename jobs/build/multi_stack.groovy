d8e8d2b47ef1213b24e1d51e4318aa3fd9a45e87
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-06 22:22:35 +0500

    Adding working code

--- jobs/build/multi_stack.groovy
+++ jobs/build/multi_stack.groovy
@@ -1,53 +1,19 @@
-// multibranchPipelineJob('multi_test1') {
-//     branchSources {
-//         git {
-//             id('123456789') // IMPORTANT: use a constant and unique identifier
-//             remote {
-//                 github('https://github.com/daniyalAhmed-code/Jenkins-JobDsl.git')
-//                 refspec('+refs/pull/*:refs/remotes/origin/pr/*')
-//             }
-//           branch('${sha1}')
-//         }
-//     }
-//      triggers {
-//         pullRequest {
-//             admin('user_1')
-//             triggerPhrase('OK to test')
-//             onlyTriggerPhrase()
-//             useGitHubHooks()
-//             permitAll()
-//             autoCloseFailedPullRequests()
-//             displayBuildErrorsOnDownstreamBuilds()
-//             whiteListTargetBranches(['develop','devTest'])
-//             allowMembersOfWhitelistedOrgsAsAdmin()
-//             extensions {
-//                 commitStatus {
-//                     context('deploy to staging site')
-//                     triggeredStatus('starting deployment to staging site...')
-//                     startedStatus('deploying to staging site...')
-//                     addTestResults(true)
-//                     completedStatus('SUCCESS', 'All is well')
-//                 }
-//             }
-//         }
-//     }
-//     publishers {
-//         mergeGithubPullRequest {
-//             mergeComment('merged by Jenkins')
-//             onlyAdminsMerge()
-//         }
-//     }
-
-//     factory {
-//         workflowBranchProjectFactory {
-//                scriptPath("pipeline/PrintText2.groovy")
-//     }
-//     }
-    
-    
-//     orphanedItemStrategy {
-//         discardOldItems {
-//             numToKeep(2)
-//         }
-//     }
-// }
+multibranchPipelineJob('multi_test1') {
+    branchSources {
+        git {
+            id('adeaf87d-d896-48ea-b207-fc3f755a9ecc')
+            remote('https://github.com/daniyalAhmed-code/Jenkins-JobDsl.git')
+            includes('develop')
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+               scriptPath("pipeline/PrintText2.groovy")
+    }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(2)
+        }
+    }
+}
