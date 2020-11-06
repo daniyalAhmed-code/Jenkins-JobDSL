73bdd794e5af16dac04caeb5a310511b6c8c0a64
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-07 00:22:54 +0500

    Testing change

--- jobs/build/multi_stack.groovy
+++ jobs/build/multi_stack.groovy
@@ -6,11 +6,18 @@ multibranchPipelineJob('multi_test1') {
                     id('23232323') // IMPORTANT: use a constant and unique identifier
                     repoOwner('daniyalAhmed-code')
                     repository('Jenkins-JobDsl')
-                    repositoryUrl('https://github.com/daniyalAhmed-code/Jenkins-JobDsl.git')
-                    configuredByUrl(false)
-                    buildOriginBranch(true)
-                    buildOriginBranchWithPR(true)
 
+                traits {
+                    gitHubBranchDiscovery {
+                    // Determines which branches are discovered.
+                    strategyId(1)
+                    }
+                    // Discovers pull requests where the origin repository is the same as the target repository.
+                    gitHubPullRequestDiscovery {
+                    // Determines how pull requests are discovered: Merging the pull request with the current target branch revision Discover each pull request once with the discovered revision corresponding to the result of merging with the current revision of the target branch.
+                    strategyId(1)
+                    }
+                }
                 }
     }
     configure {
