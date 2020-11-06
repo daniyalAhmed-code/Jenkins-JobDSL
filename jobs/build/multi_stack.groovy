845dd50ea07e2a6d2ae068ee2639f417dd4a18b8
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-06 22:51:28 +0500

    Testing change

--- jobs/build/multi_stack.groovy
+++ jobs/build/multi_stack.groovy
@@ -6,47 +6,22 @@ multibranchPipelineJob('multi_test1') {
              includes('develop')
         }
     }
-    
+     configure {
+        it / 'triggers' << 'com.cloudbees.hudson.plugins.folder.computed.PeriodicFolderTrigger' {
+            spec '* * * * *'
+            interval "60000"
+        }
+        it / factory(class: "org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory") << {
+            scriptPath("pipelines/dev.groovy")
+        }     
+    }
   
     factory {
         workflowBranchProjectFactory {
                scriptPath("pipeline/PrintText2.groovy")
     }
     }
-     configure {
-        it / sources / 'data' / 'jenkins.branch.BranchSource' << {
-            source(class: 'jenkins.plugins.git.GitSCMSource') {
-                id(uuid)
-                remote("https://github.com/daniyalAhmed-code/Jenkins-JobDsl.git")
-                includes('*')
-                excludes('')
-                ignoreOnPushNotifications('false')
-                traits {
-                    'jenkins.plugins.git.traits.BranchDiscoveryTrait'()
-                }
-            }
-            // default strategy when sourcing from a branch
-            strategy(class: "jenkins.branch.NamedExceptionsBranchPropertyStrategy") {
-                defaultProperties(class: "java.util.Arrays\$ArrayList") {
-                    a(class: "jenkins.branch.BranchProperty-array") {
-                        // don't trigger builds
-                        "jenkins.branch.NoTriggerBranchProperty"()
-                    }
-                }
-                // exceptions to the default strategy
-                namedExceptions(class: "java.util.Arrays\$ArrayList") {
-                    a(class: "jenkins.branch.NamedExceptionsBranchPropertyStrategy\$Named-array") {
-                        "jenkins.branch.NamedExceptionsBranchPropertyStrategy_-Named"() {
-                            // for the brach named `master` trigger builds 
-                            //   (this is default behaviour if no branch properties are specified)
-                            props(class: "empty-list")
-                            name("master")
-                        }
-                    }
-                }
-            }
-        }
-     }
+    
     orphanedItemStrategy {
         discardOldItems {
             numToKeep(2)
