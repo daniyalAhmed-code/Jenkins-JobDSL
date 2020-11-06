e06a53d1d9ad9c8b02cd4d290086d11ac59bc1f6
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-06 22:59:09 +0500

    Testing change

--- jobs/build/multi_stack.groovy
+++ jobs/build/multi_stack.groovy
@@ -1,44 +1,27 @@
-    multibranchPipelineJob("my_awesome_job") {
-    displayName "my awesome job"
-    description "multi-branch pipeline job thingy"
-    configure {
-        it / sources / 'data' / 'jenkins.branch.BranchSource' << {
-            source(class: 'jenkins.plugins.git.GitSCMSource') {
-                id(uuid)
-                remote("https://github.com/daniyalAhmed-code/Jenkins-JobDsl.git")
-                includes('*')
-                excludes('')
-                ignoreOnPushNotifications('false')
-                traits {
-                    'jenkins.plugins.git.traits.BranchDiscoveryTrait'()
+multibranchPipelineJob('multi_test1') {
+   branchSources {
+        branchSource {
+            source {
+                git {
+                    remote ('https://github.com/daniyalAhmed-code/Jenkins-JobDsl.git')
                 }
             }
-            // default strategy when sourcing from a branch
-            strategy(class: "jenkins.branch.NamedExceptionsBranchPropertyStrategy") {
-                defaultProperties(class: "java.util.Arrays\$ArrayList") {
-                    a(class: "jenkins.branch.BranchProperty-array") {
-                        // don't trigger builds
-                        "jenkins.branch.NoTriggerBranchProperty"()
-                    }
-                }
-                // exceptions to the default strategy
-                namedExceptions(class: "java.util.Arrays\$ArrayList") {
-                    a(class: "jenkins.branch.NamedExceptionsBranchPropertyStrategy\$Named-array") {
-                        "jenkins.branch.NamedExceptionsBranchPropertyStrategy_-Named"() {
-                            // for the brach named `master` trigger builds 
-                            //   (this is default behaviour if no branch properties are specified)
-                            props(class: "empty-list")
-                            name("master")
-                        }
+            strategy {
+                defaultBranchPropertyStrategy {
+                    props {
+                        noTriggerBranchProperty()
                     }
                 }
             }
-        }  
-        // customise the branch project factory
-        it / factory(class: "org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory") << {
-            // pipeline jobs will have their script path set to `pipelines/customPipeline.groovy`
-            scriptPath("Jenkinsfile")
-        } 
+        }
     }
+    configure {
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' {}
+    }
+    
+    orphanedItemStrategy { discardOldItems { numToKeep(-1) } }
 }
    
+   
+    
