8896f2648ec022a6e14a04b4d5e804b5aa781183
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-07 00:10:41 +0500

    Testing change

--- jobs/build/multi_stack.groovy
+++ jobs/build/multi_stack.groovy
@@ -4,18 +4,22 @@ multibranchPipelineJob('multi_test1') {
             id('23232323') // IMPORTANT: use a constant and unique identifier
              repoOwner('daniyalAhmed-code')
             repository('Jenkins-JobDsl')
-        withTraits {
-              // discover PRs
-              originPullRequestDiscoveryTrait {
-                strategyId(1) //build both the head and merge refs
-              }
+        
         }
     }
-    
+    configure {
+      def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+      traits << 'com.cloudbees.jenkins.plugins.bitbucket.BranchDiscoveryTrait' {
+        strategyId(3) // detect all branches
+      }
+      traits << 'com.cloudbees.jenkins.plugins.bitbucket.OriginPullRequestDiscoveryTrait' {
+      strategyId(3) // detect all PRs
+    }
+    }
     orphanedItemStrategy {
         discardOldItems {
             numToKeep(2)
         }
     }
-}}
+}
 
