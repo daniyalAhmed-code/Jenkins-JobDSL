d1cf74efe2c0d4d83d6df47dac1222c22309b56e
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-06 23:47:48 +0500

    Testing change

--- jobs/build/multi_stack.groovy
+++ jobs/build/multi_stack.groovy
@@ -6,7 +6,15 @@ multibranchPipelineJob('multi_test1') {
             repository('Jenkins-JobDsl')
         }
     }
-    
+    configure {
+    def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+    traits << 'com.cloudbees.jenkins.plugins.bitbucket.BranchDiscoveryTrait' {
+      strategyId(1)
+    }
+    traits << 'com.cloudbees.jenkins.plugins.bitbucket.OriginPullRequestDiscoveryTrait' {
+      strategyId(1)
+    }
+  }
     orphanedItemStrategy {
         discardOldItems {
             numToKeep(2)
