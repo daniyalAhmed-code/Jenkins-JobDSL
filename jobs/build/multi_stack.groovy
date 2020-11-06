2ee1b1af65372b0b3def4093c9c3e1530234c8dc
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-11-06 23:25:21 +0500

    Testing change

--- jobs/build/multi_stack.groovy
+++ jobs/build/multi_stack.groovy
@@ -2,8 +2,7 @@ multibranchPipelineJob('multi_test1') {
     branchSources {
         github {
             id('123456789') // IMPORTANT: use a constant and unique identifier
-            remote {
-                url('https://github.com/ThePillClub/jenkins.git')
+            getAt('https://github.com/ThePillClub/jenkins.git')
        
         }
     }
@@ -14,4 +13,4 @@ multibranchPipelineJob('multi_test1') {
         }
     }
 }
-}
+
